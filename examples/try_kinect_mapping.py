17e3a0266d82bf254a565d1719ac6d4f437af27a
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-18 18:16:19 +0000

    Make the program terminate as ordered.

--- examples/try_kinect_mapping.py
+++ examples/try_kinect_mapping.py
@@ -1,10 +1,16 @@
+import os
+
+from openni import openni2
+
 from smg.openni.openni_camera import OpenNICamera
 from smg.pyorbslam2 import RGBDTracker
 from smg.pyremode import DepthEstimator, RGBDMappingSystem, RGBDOpenNICamera, TemporalKeyframeDepthEstimator
 
 
 def main():
-    with OpenNICamera(mirror_images=True) as camera:
+    openni2.initialize()
+
+    with OpenNICamera(manage_openni=False, mirror_images=True) as camera:
         with RGBDTracker(
             settings_file=f"settings-kinect.yaml", use_viewer=True,
             voc_file="C:/orbslam2/Vocabulary/ORBvoc.txt", wait_till_ready=False
@@ -14,6 +20,9 @@ def main():
             )
             with RGBDMappingSystem(RGBDOpenNICamera(camera), tracker, depth_estimator) as system:
                 system.run()
+
+            # noinspection PyProtectedMember
+            os._exit(0)
 
 
 if __name__ == "__main__":
