23c8fe71a6298065c86d7a729f95e01829636f31
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-18 13:03:16 +0000

    Add yet more comments.

--- smg/pyremode/python/temporal_keyframe_depth_estimator.py
+++ smg/pyremode/python/temporal_keyframe_depth_estimator.py
@@ -39,23 +39,16 @@ class TemporalKeyframeDepthEstimator(DepthEstimator):
         self.__lock = threading.Lock()
         self.__keyframe_ready = threading.Condition(self.__lock)
 
-    # SPECIAL METHODS
-
-    def __enter__(self):
-        """TODO"""
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """TODO"""
-        self.terminate()
-
     # PUBLIC METHODS
 
     def get(self) -> Optional[Tuple[np.ndarray, np.ndarray, np.ndarray, float, np.ndarray]]:
         """
-        TODO
+        Try to get the images, pose and convergence % / map for an assembled keyframe.
 
-        :return:    TODO
+        .. note::
+            This is intended to be blocking, and should only return None when the estimator has been told to terminate.
+
+        :return:    The images, pose and ocnvergence % / map for an assembled keyframe, if possible, or None otherwise.
         """
         with self.__lock:
             while not self.__keyframe_is_ready:
@@ -68,7 +61,18 @@ class TemporalKeyframeDepthEstimator(DepthEstimator):
 
     def put(self, input_colour_image: np.ndarray, input_pose: np.ndarray) -> None:
         """
-        Try to add a colour image with a known pose to the depth estimator.
+        Try to pass a colour image with a known pose to the depth estimator.
+
+        .. note::
+            This is intended to be non-blocking, for performance reasons, and so any particular image passed in
+            may in practice be silently dropped rather than being used as part of the depth estimation process.
+            The intended use case is to pass in a stream of images from a camera, and let the depth estimator
+            use the ones it can. It is of course possible to write a depth estimator that uses every image that's
+            passed to it, if desired, but that would necessarily involve blocking if the estimator can't keep up,
+            which would slow down the calling thread. An alternative approach of buffering the images that are
+            passed in until the estimator's ready for them is also possible, but with finite memory, there's
+            eventually going to come a point where an estimator that can't keep up has to drop some images.
+            Given this, a non-blocking put function with no buffering seems best.
 
         :param input_colour_image:  The input colour image.
         :param input_pose:          The input camera pose (denoting a transformation from camera space to world space).
