90eb3be75155347b0d4628a783cb01a069663664
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-19 17:24:32 +0000

    Continue debugging what's wrong with depth from the Tello.

--- examples/try_drone_depth_assembly.py
+++ examples/try_drone_depth_assembly.py
@@ -3,47 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
 
-from scipy.spatial.transform import Rotation
-from typing import Optional
+from typing import Optional, Tuple
 
 from smg.open3d import VisualisationUtil
-from smg.pyopencv import CVMat1b
 from smg.pyorbslam2 import MonocularTracker
-from smg.utility import GeometryUtil
-
-from smg.pyremode import *
 from smg.rotory.drone_factory import DroneFactory
 
-
-def print_se3(se3: SE3f) -> None:
-    print()
-    for row in range(3):
-        print([se3.data(row, col) for col in range(4)])
-    print()
+from smg.pyremode import CONVERGED, DepthAssembler
+from smg.utility import GeometryUtil
 
 
 def main():
     with DroneFactory.make_drone("tello", local_ip="192.168.10.3") as drone:
         with MonocularTracker(
-            settings_file=f"settings-tello.yaml", use_viewer=True,
+            settings_file=f"settings-kinect.yaml", use_viewer=True,
             voc_file="C:/orbslam2/Vocabulary/ORBvoc.txt", wait_till_ready=False
         ) as tracker:
-            # fx, fy, cx, cy = 946.60441222, 941.38386885, 460.29254907, 357.08431882
-            fx, fy, cx, cy = 938.55289501, 932.86950291, 480.0, 360.0
-            depthmap: Depthmap = Depthmap(960, 720, fx, cx, fy, cy)
+            colour_dims: Tuple[int, int] = (960, 720)
+            intrinsics: Tuple[float, float, float, float] = (938.55289501, 932.86950291, 480.0, 360.0)
+            depth_assembler: DepthAssembler = DepthAssembler(colour_dims, intrinsics)
+            is_keyframe: bool = True
+
             reference_colour_image: Optional[np.ndarray] = None
+            estimated_depth_image: Optional[np.ndarray] = None
+            convergence_map: Optional[np.ndarray] = None
 
             _, ax = plt.subplots(2, 2)
 
             while True:
+                # TODO
                 distorted_image = drone.get_image()
-
-                # camera_matrix: np.ndarray = np.array([
-                #     [946.60441222, 0., 460.29254907],
-                #     [0., 941.38386885, 357.08431882],
-                #     [0., 0., 1.]
-                # ])
-                # dist_coeffs: np.ndarray = np.array([[0.04968041, -0.59998154, -0.00377696, -0.00863985, 2.14472665]])
                 camera_matrix: np.ndarray = np.array([
                     [938.55289501, 0., 480.],
                     [0., 932.86950291, 360.],
@@ -55,41 +44,36 @@ def main():
                 cv2.imshow("Image", colour_image)
                 cv2.waitKey(1)
 
+                # TODO
                 if not tracker.is_ready():
                     continue
                 pose: Optional[np.ndarray] = tracker.estimate_pose(colour_image)
                 if pose is None:
                     continue
 
-                r: Rotation = Rotation.from_matrix(pose[0:3, 0:3])
-                t: np.ndarray = pose[0:3, 3]
-                qx, qy, qz, qw = r.as_quat()
-                se3: SE3f = SE3f(qw, qx, qy, qz, *t)
+                # TODO
+                depth_assembler.put(colour_image, pose, blocking=False)
 
-                # print_se3(se3)
-
-                grey_image: np.ndarray = cv2.cvtColor(colour_image, cv2.COLOR_BGR2GRAY)
-                cv_grey_image: CVMat1b = CVMat1b.zeros(*grey_image.shape[:2])
-                np.copyto(np.array(cv_grey_image, copy=False), grey_image)
-
-                if reference_colour_image is None:
+                # TODO
+                if is_keyframe:
                     reference_colour_image = colour_image
-                    depthmap.set_reference_image(cv_grey_image, se3, 0.1, 4.0)
-                else:
-                    depthmap.update(cv_grey_image, se3)
+                    is_keyframe = False
 
-                estimated_depth_image: np.ndarray = np.array(depthmap.get_denoised_depthmap())
-                convergence_map: np.ndarray = np.array(depthmap.get_convergence_map())
+                # TODO
+                result = depth_assembler.get(blocking=False)
+                if result is not None:
+                    _, estimated_depth_image, _, converged_percentage, convergence_map = result
+                    print(f"Converged %: {converged_percentage}")
 
-                print(depthmap.get_converged_percentage())
-
+                # TODO
                 ax[0, 0].clear()
                 ax[0, 1].clear()
                 ax[1, 0].clear()
                 ax[1, 1].clear()
-                ax[0, 0].imshow(reference_colour_image)
-                ax[0, 1].imshow(estimated_depth_image)  # , vmin=0.0, vmax=4.0)
-                ax[1, 0].imshow(colour_image)
+                ax[0, 0].imshow(reference_colour_image[:, :, [2, 1, 0]])
+                if estimated_depth_image is not None:
+                    ax[0, 1].imshow(estimated_depth_image, vmin=0.0, vmax=4.0)
+                ax[1, 0].imshow(colour_image[:, :, [2, 1, 0]])
                 # ax[1, 1].imshow(reference_depth_image, vmin=0.0, vmax=4.0)
 
                 plt.draw()
@@ -102,7 +86,7 @@ def main():
             # TODO
             depth_mask: np.ndarray = np.where(convergence_map == CONVERGED, 255, 0).astype(np.uint8)
             pcd_points, pcd_colours = GeometryUtil.make_point_cloud(
-                reference_colour_image, estimated_depth_image, depth_mask, (fx, fy, cx, cy)
+                reference_colour_image, estimated_depth_image, depth_mask, intrinsics
             )
 
             # Convert the point cloud to Open3D format.
