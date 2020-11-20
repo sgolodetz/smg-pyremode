5ca2168ba17b920c6db3d816b0762f534aedcbae
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-20 16:59:29 +0000

    Introduce the DepthDenoiser module.

--- examples/try_kinect_depth_assembly.py
+++ examples/try_kinect_depth_assembly.py
@@ -8,78 +8,8 @@ from typing import Optional, Tuple
 from smg.open3d import VisualisationUtil
 from smg.openni import OpenNICamera
 from smg.pyorbslam2 import RGBDTracker
-from smg.pyremode import CONVERGED, DepthAssembler
+from smg.pyremode import CONVERGED, DepthAssembler, DepthDenoiser
 from smg.utility import GeometryUtil
-
-
-def denoise_depth(depth_image: np.ndarray, intrinsics: Tuple[float, float, float, float]) \
-        -> np.ndarray:
-    """
-    Denoise the specified depth image.
-
-    .. note::
-        The technique currently used is to:
-
-          (i) Make a point cloud.
-         (ii) Downsample the point cloud and remove statistical outliers using Open3D.
-        (iii) Use the inliers found by Open3D to make a new depth image.
-         (iv) Dilate the new depth image to restore some of its original density.
-
-        Various improvements may be made in the future, e.g. restoring the estimated depths of the pixels
-        surrounding the known inliers if they're close enough to the inlier depth to be trusted.
-
-    :param depth_image: The depth image.
-    :param intrinsics:  The camera intrinsics.
-    :return:            The denoised depth image.
-    """
-    height, width = depth_image.shape
-    colour_image: np.ndarray = np.zeros((height, width, 3), dtype=np.uint8)
-    depth_mask: np.ndarray = np.where(depth_image != 0, 255, 0).astype(np.uint8)
-
-    # Make the original point cloud.
-    pcd_points, pcd_colours = GeometryUtil.make_point_cloud(colour_image, depth_image, depth_mask, intrinsics)
-
-    # Shuffle the points to avoid artifacts when the point cloud is uniformly downsampled.
-    rng_state = np.random.get_state()
-    original_indices: np.ndarray = np.array(range(len(pcd_points)))
-    np.random.shuffle(original_indices)
-    np.random.set_state(rng_state)
-    np.random.shuffle(pcd_points)
-    np.random.set_state(rng_state)
-    np.random.shuffle(pcd_colours)
-
-    # Convert the original point cloud to Open3D format.
-    pcd: o3d.geometry.PointCloud = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(pcd_points)
-    pcd.colors = o3d.utility.Vector3dVector(pcd_colours)
-
-    # Downsample the original point cloud and denoise the downsampled point cloud.
-    factor: int = 10
-    pcd = pcd.uniform_down_sample(every_k_points=factor)
-    pcd, ind = pcd.remove_statistical_outlier(20, 2.0)
-
-    # Remap the indices of the retained points to correspond to the points in the original point cloud.
-    ind = list(map(lambda x: original_indices[x * factor], ind))
-
-    # Convert the retained point indices into a mask.
-    ind_mask: np.ndarray = np.zeros(depth_image.shape, dtype=np.uint8)
-    ind_mask = ind_mask.flatten()
-    ind_mask[ind] = 255
-    ind_mask = ind_mask.reshape(depth_image.shape, order='C')
-
-    # Mask out the outliers in the depth image.
-    depth_image = np.where(ind_mask != 0, depth_image, 0.0).astype(np.float32)
-
-    # Convert the floating-point depth image to a short depth image so that it can be dilated.
-    depth_scale_factor: float = 1000.0
-    scaled_depth_image: np.ndarray = (depth_image * depth_scale_factor).astype(np.uint16)
-
-    # Dilate the short depth image.
-    kernel: np.ndarray = np.ones((5, 5), np.uint8)
-    scaled_depth_image = cv2.dilate(scaled_depth_image, kernel)
-
-    # Convert the dilated short depth image back to a floating-point depth image and return it.
-    return scaled_depth_image.astype(np.float32) / depth_scale_factor
 
 
 def main():
@@ -148,7 +78,7 @@ def main():
             # TODO
             depth_mask: np.ndarray = np.where(convergence_map == CONVERGED, 255, 0).astype(np.uint8)
             estimated_depth_image = np.where(depth_mask != 0, estimated_depth_image, 0.0).astype(np.float32)
-            estimated_depth_image = denoise_depth(estimated_depth_image, intrinsics)
+            estimated_depth_image = DepthDenoiser.denoise_depth(estimated_depth_image, intrinsics)
 
             plt.imshow(estimated_depth_image, vmin=0.0, vmax=4.0)
             plt.waitforbuttonpress()
