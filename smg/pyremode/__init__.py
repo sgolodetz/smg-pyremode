33214412f82bc4fbfc079c2a27f912749acdb346
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-24 23:06:11 +0000

    Reorganise things a bit.

--- smg/pyremode/__init__.py
+++ smg/pyremode/__init__.py
@@ -1,16 +1,16 @@
 from .cpp.pyremode import *
 
-from .python.depth_assembler import DepthAssembler
-from .python.depth_estimator import DepthEstimator
-from .python.depth_processor import DepthProcessor
-from .python.temporal_keyframe_depth_estimator import TemporalKeyframeDepthEstimator
+from .python.depthestimation.depth_assembler import DepthAssembler
+from .python.depthestimation.depth_estimator import DepthEstimator
+from .python.depthestimation.depth_processor import DepthProcessor
+from .python.depthestimation.temporal_keyframe_depth_estimator import TemporalKeyframeDepthEstimator
 
-from .python.rgb_image_source import RGBImageSource
-from .python.rgbd_image_source import RGBDImageSource
+from .python.imagesources.rgb_image_source import RGBImageSource
+from .python.imagesources.rgbd_image_source import RGBDImageSource
 
-from .python.rgb_drone_camera import RGBDroneCamera
-from .python.rgb_from_rgbd_image_source import RGBFromRGBDImageSource
-from .python.rgbd_openni_camera import RGBDOpenNICamera
+from .python.imagesources.rgb_drone_camera import RGBDroneCamera
+from .python.imagesources.rgb_from_rgbd_image_source import RGBFromRGBDImageSource
+from .python.imagesources.rgbd_openni_camera import RGBDOpenNICamera
 
-from .python.monocular_mapping_system import MonocularMappingSystem
-from .python.rgbd_mapping_system import RGBDMappingSystem
+from .python.mapping.monocular_mapping_system import MonocularMappingSystem
+from .python.mapping.rgbd_mapping_system import RGBDMappingSystem
