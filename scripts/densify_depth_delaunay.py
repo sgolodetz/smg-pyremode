607343c8ba6df896b2cfed854f9232f89e4c27b8
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-24 17:16:27 +0000

    Tidy up another script.

--- scripts/densify_depth_delaunay.py
+++ scripts/densify_depth_delaunay.py
@@ -14,9 +14,13 @@ def main():
     parser.add_argument("--output_file", "-o", type=str, required=True, help="the output file")
     args: dict = vars(parser.parse_args())
 
+    # Load in the input depth image.
     input_depth_image: np.ndarray = ImageUtil.load_depth_image(args["input_file"])
+
+    # Densify it using the Delaunay triangulation approach.
     output_depth_image, triangulation = DepthProcessor.densify_depth_delaunay(input_depth_image)
 
+    # Visualise the results.
     _, ax = plt.subplots(1, 3)
     ax[0].imshow(input_depth_image, vmin=0.0, vmax=4.0)
     ax[1].imshow(output_depth_image, vmin=0.0, vmax=4.0)
